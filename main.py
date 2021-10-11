3283f334ce1c8fcc1bbd1a473f553bc10ffdc444
Alexandru Lupei <lupei.alexandru2001@gmail.com>
2021-10-11 18:49:59 +0300

    Modificare tema lab 3 ~ Problema 3

--- main.py
+++ main.py
@@ -59,6 +59,44 @@ def get_longest_equal_int_real(lst: list[float]) -> list[float]:
     return subsecventamax
 
 
+def is_palindrome(n):
+    '''
+    Verifica daca un numar este palindrom
+    :param n:  numar intreg
+    :return: Retruneaza adevarat daca nr este palindrom si False in caz contrar
+    '''
+    if n < 10:
+        return False
+    ogl = 0
+    aux = n
+    while aux > 0:
+        ogl = ogl * 10 + aux % 10
+        aux = aux // 10
+    if ogl == n:
+        return True
+    else:
+        return False
+
+def  get_longest_all_palindromes(lst: list[int]) -> list[int]:
+    '''
+    Determina cea mai lunga subsecventa de numere cu popr. ca toate nr sunt palindrom
+    :param lst: lista cu numere reale
+    :return: cea mai lunga subsecventa cu proprietatea ca toate numerele sunt palindrom
+    '''
+    subsecventamax = []
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if is_palindrome(lst[i:j + 1]) and len(lst[i:j + 1]) > len(subsecventamax):
+                subsecventamax = lst[i:j + 1]
+    return subsecventamax
+
+
+def test_get_longest_all_palindromes():
+    assert get_longest_all_palindromes([]) == []
+    assert get_longest_all_palindromes([12, 11, 22, 44, 54, 22]) == [11, 22, 44]
+    assert get_longest_all_palindromes([12, 11, 22, 44, 54, 22, 66, 101, 202]) == [22, 66, 101, 202]
+
+
 def test_elemordonatecresc():
     assert elemordonatecresc([]) is True
     assert elemordonatecresc([10, 4, 5]) is False
@@ -84,6 +122,7 @@ def main():
     print("""
     1, Determinati cea mai lunga subsecventa de numere ordonate crescator dintr un sir dat
     2, Determinati cea mai lunga subsecventa cu proprietatea ca elementele au partea fractionara egala cu partea intreaga
+    3, Determinati cea mai lunga subsecventa cu proprietatea ca toate elem sunt palindrom
     x, Iesire""")
     while True:
         option = input("Selectati o funtie")
@@ -93,6 +132,9 @@ def main():
         elif option == "2":
             l = readfloatlist()
             print(get_longest_equal_int_real(l))
+        elif option == '3':
+            l = readintlist()
+            print(get_longest_all_palindromes(l))
         elif option == "x":
             break
         else:
